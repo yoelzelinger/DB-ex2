667b421cf94edeba920fa307e8a381a25484cf75
yoel.zelinger <yoel.zelinger@mail.huji.ac.il>
2024-06-04 15:02:45 +0300

    started

---
+++ correct.sql
@@ -0,0 +1,13 @@
+SELECT
+    c1.cid AS cid1,
+    c2.cid AS cid2
+FROM
+    (SELECT cid, STRING_AGG(preid, ',' order by preid) AS PrereqList
+     FROM Prerequisites
+     GROUP BY cid) AS c1
+JOIN
+    (SELECT cid, STRING_AGG(preid, ',' order by preid) AS PrereqList
+     FROM Prerequisites
+     GROUP BY cid) AS c2
+ON c1.PrereqList = c2.PrereqList AND c1.cid < c2.cid
+ORDER BY c1.cid, c2.cid;
---
+++ example.sql
@@ -0,0 +1,6 @@
+INSERT INTO Course VALUES ('1', 'Computer Science');
+INSERT INTO Course VALUES ('2', 'Computer Science');
+INSERT INTO Prerequisites VALUES ('1', '1');
+INSERT INTO Prerequisites VALUES ('1', '2');
+INSERT INTO Prerequisites VALUES ('2', '2');
+INSERT INTO Prerequisites VALUES ('2', '1');
